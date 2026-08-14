c748f9652da95e07abdbe527560ff0a000cf018d
Yuki Omae <omyk@omyk-surface>
2026-08-14 13:48:02 +0900

    ✨ Cloudflare経由の独自ドメイン配信を追加

--- infra/terraform/environments/dev/providers.tf
+++ infra/terraform/environments/dev/providers.tf
@@ -9,3 +9,16 @@ provider "aws" {
     }
   }
 }
+
+provider "aws" {
+  alias  = "us_east_1"
+  region = "us-east-1"
+
+  default_tags {
+    tags = {
+      Project     = var.project_name
+      Environment = var.environment
+      ManagedBy   = "terraform"
+    }
+  }
+}
